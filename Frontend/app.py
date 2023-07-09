248a7ec85c7893516fc718a87a3712818fca197a
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2023-07-09 19:28:57 -0400

    pushing frontend with matching engine defined

--- Frontend/app.py
+++ Frontend/app.py
@@ -18,7 +18,12 @@ from langchain.prompts.chat import (
 from langchain.schema import HumanMessage, SystemMessage
 from langchain import PromptTemplate, LLMChain
 
+from langchain.chains import ConversationalRetrievalChain
+
 from langchain.vectorstores import MatchingEngine
+from langchain.embeddings import VertexAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+
 
 # os.environ["OPENAI_API_KEY"] = "YOUR_OPEN_AI_API_KEY"
 
@@ -26,24 +31,50 @@ template = """Question: {question}
 
 Answer: Let's think step by step."""
 
-# Getting the Hackathon credentials from the environment variable
-hack_credentials = os.getenv('credentials')
-service_account_info = json.loads(hack_credentials)
-hackathon_project_id = service_account_info["project_id"]
+
+with open(
+    "vertex-hackathon-creds.json", encoding="utf-8"
+) as f:  # replace 'serviceAccount.json' with the path to your file if necessary
+    service_account_info = json.load(f)
+    our_project_id = service_account_info["project_id"]
+
+
+our_credentials = service_account.Credentials.from_service_account_info(
+    service_account_info
+)
+
+
+with open(
+    "credentials.json", encoding="utf-8"
+) as f:  # replace 'serviceAccount.json' with the path to your file if necessary
+    service_account_info = json.load(f)
+    hackathon_project_id = service_account_info["project_id"]
+
 
 hackathon_credentials = service_account.Credentials.from_service_account_info(
     service_account_info
 )
 
 
-# Getting our own account credentials from the environment variable
-our_creds = os.getenv('our_creds')
-our_service_account_info = json.loads(our_creds)
-our_project_id = our_service_account_info["project_id"]
+# Getting the Hackathon credentials from the environment variable
 
-our_credentials = service_account.Credentials.from_service_account_info(
-    our_service_account_info
-)
+# hack_credentials = os.getenv('credentials')
+# service_account_info = json.loads(hack_credentials)
+# hackathon_project_id = service_account_info["project_id"]
+
+# hackathon_credentials = service_account.Credentials.from_service_account_info(
+#     service_account_info
+# )
+
+
+# Getting our own account credentials from the environment variable
+# our_creds = os.getenv('our_creds')
+# our_service_account_info = json.loads(our_creds)
+# our_project_id = our_service_account_info["project_id"]
+
+# our_credentials = service_account.Credentials.from_service_account_info(
+#     our_service_account_info
+# )
 
 
 template = """Question: {question}
@@ -58,14 +89,37 @@ Answer: Let's think step by step."""
 # print(llm_chain.run(question))
 
 
+embeddings = VertexAIEmbeddings(credentials=hackathon_credentials, project=hackathon_project_id)
+
+gcs_bucket_name = MatchingEngine._validate_gcs_bucket("vertexai-hackathon-embeddings")
+# credentials = MatchingEngine._create_credentials_from_file(credentials_path)
+index = MatchingEngine._create_index_by_id("279847699501547520", our_project_id, "us-central1", our_credentials)
+endpoint = MatchingEngine._create_endpoint_by_id("6715491567013986304", our_project_id, "us-central1", our_credentials)
+
+gcs_client = MatchingEngine._get_gcs_client(our_credentials, our_project_id)
+MatchingEngine._init_aiplatform(our_project_id, "us-central1", gcs_bucket_name, credentials)
+
+
+vector_store =  MatchingEngine(project_id=our_project_id,
+                    index=index,
+                    endpoint=endpoint,
+                    embedding=embeddings,
+                    gcs_client=gcs_client,
+                    credentials=our_credentials,
+                    gcs_bucket_name=gcs_bucket_name,
+                )
+
+# Memory for the chat
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
 
 @cl.langchain_factory(use_async=True)
 def factory():
     # prompt = PromptTemplate(template=template, input_variables=["question"])
     # llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
     prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm = VertexAI(credentials=hackathon_credentials, project=hackathon_project_id, location="us-central1", model_name="text-bison@001", temperature=0.5, max_output_tokens=1024, top_p=0.95, top_k=40)
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    llm = ChatVertexAI(credentials=hackathon_credentials, project=hackathon_project_id, location="us-central1", model_name="chat-bison", temperature=0.5, max_output_tokens=1024, top_p=0.95, top_k=40)
+    llm_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), verbose=True, memory=memory)
 
 
     return llm_chain
--- Python/test5.py
+++ Python/test5.py
@@ -19,6 +19,7 @@ from langchain.schema import HumanMessage, SystemMessage
 from langchain import PromptTemplate, LLMChain
 
 from langchain.vectorstores import MatchingEngine
+from langchain.embeddings import VertexAIEmbeddings
 
 
 with open(
@@ -31,23 +32,58 @@ with open(
 our_credentials = service_account.Credentials.from_service_account_info(
     service_account_info
 )
+embeddings = VertexAIEmbeddings(credentials=our_credentials, project=project_id)
+
+vector_store = MatchingEngine.from_components(
+    project_id="<my_project_id>",
+    region="<my_region>",
+    gcs_bucket_uri="<my_gcs_bucket>",
+    index_id="<my_matching_engine_index_id>",
+    endpoint_id="<my_matching_engine_endpoint_id>",
+    embedding=embeddings,
+)
+
 
 
 
 
 def create_deploy_endpoint():
-    my_index_endpoint = aiplatform.MatchingEngineIndexEndpoint.create(
-        credentials=our_credentials,
-        display_name=f"public-test-endpoint",
-        public_endpoint_enabled = True,
-    )
+    # print("Creating index ...")
+    # my_index = aiplatform.MatchingEngineIndex.create_tree_ah_index(
+    #     credentials = our_credentials,
+    #     display_name="vertex-hackathon-index",
+    #     contents_delta_uri="gs://vertexai-hackathon-embeddings/batch_root",
+    #     dimensions=768,
+    #     approximate_neighbors_count=150,
+    #     distance_measure_type="COSINE_DISTANCE",
+    #     leaf_node_embedding_count = 5000,
+    #     leaf_nodes_to_search_percent = 3,
+    #     location = "us-central1",
+    # )
     
-    my_index_endpoint = my_index_endpoint.deploy_index(
-    index='vertexai_test_index', deployed_index_id='4847060671608651776'
-    )
-
-    print("Deployed indices are:", my_index_endpoint.deployed_indexes)
-    print("Deployed index endpoint is:", my_index_endpoint)
-    return my_index_endpoint
+    # print("Index created:", my_index)
+    # print("Creating index endpoint ...")
+    
+    # my_index_endpoint = aiplatform.MatchingEngineIndexEndpoint.create(
+    #     credentials=our_credentials,
+    #     display_name=f"public-test-endpoint",
+    #     public_endpoint_enabled = True,
+    # )
+    # print("Index endpoint created:", my_index_endpoint)
+    # print("Deploying index to endpoint ...")
+    
+    # my_index_endpoint = my_index_endpoint.deploy_index(
+    # index=my_index,
+    # deployed_index_id='4847060671608651776'
+    # )
+    # print("Deployed indices are:", my_index_endpoint.deployed_indexes)
+    # print("Deployed index endpoint is:", my_index_endpoint)
+    print("Project ID is->>>>>>>>>>>> ",project_id)
+    my_index = aiplatform.MatchingEngineIndex(index_name='projects/287955880258/locations/us-central1/indexes/279847699501547520', credentials=our_credentials, location='us-central1')
+    # print(my_index.deployed_indexes) 
+    index_endpoint = aiplatform.MatchingEngineIndexEndpoint(index_endpoint_name='6715491567013986304', credentials=our_credentials, project=project_id, location='us-central1')
+    index_endpoint.deploy_index(index=my_index, deployed_index_id='test_deployed_index')
+    
+    return #my_index_endpoint
 
 create_deploy_endpoint()
